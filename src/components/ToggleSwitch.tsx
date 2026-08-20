6ba20fed85c87d2ab70b23cda584c9d9b13f5696
Emmanuel Sani <emmanuelsani2006@gmail.com>
2026-08-20 15:17:08 +0100

    fix: Fixed styling Issues with ToggleSwtich Component

--- src/components/ToggleSwitch.tsx
+++ src/components/ToggleSwitch.tsx
@@ -6,9 +6,9 @@ const ToggleMode = () => {
   return (
     <>
       <div className="flex items-center gap-2">
-        <i className="bi bi-brightness-high text-2xl" />
+        <i className="bi bi-code-slash text-2xl" />
         <Switch checked={theme === "creative"} onCheckedChange={toggleTheme} />
-        <i className="bi bi-moon text-2xl" />
+        <i className="bi bi-palette-fill text-2xl" />
       </div>
     </>
   );
