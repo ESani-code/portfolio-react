837af50e4f1f007d9e9b3417713f881033345c7e
Emmanuel Sani <emmanuelsani2006@gmail.com>
2026-08-22 13:56:53 +0100

    feat: Added Click Spark Component across the whole webpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // import { BrowserRouter } from "react-router-dom";
 
 import About from "./components/About";
+import ClickSpark from "./components/ClickSpark";
 import Hero from "./components/Hero";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
@@ -9,8 +10,16 @@ const App = () => {
     <>
       {/* <BrowserRouter> */}
 
-      <Hero />
-      <About />
+      <ClickSpark
+        sparkColor="#ff2020"
+        sparkSize={5}
+        sparkRadius={30}
+        sparkCount={9}
+        duration={400}
+      >
+        <Hero />
+        <About />
+      </ClickSpark>
 
       {/* </BrowserRouter> */}
     </>
