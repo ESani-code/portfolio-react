333d300f440f3029d999c4f50007f7b7bdd45021
Emmanuel Sani <emmanuelsani2006@gmail.com>
2026-08-27 22:34:41 +0100

    fix: Added more Graphics works

--- src/view/workone/GraphicWorks.ts
+++ src/view/workone/GraphicWorks.ts
@@ -3,18 +3,25 @@ export const work = [
     id: "1",
     img: "https://res.cloudinary.com/don03rsnm/image/upload/v1787336142/Export_rpaktx.png",
     url: "https://example.com/one",
-    height: 400,
+    height: 750,
   },
   {
     id: "2",
     img: "https://res.cloudinary.com/don03rsnm/image/upload/v1787336118/Export_2_b2h9ga.png",
     url: "https://example.com/two",
-    height: 250,
+    height: 750,
   },
   {
     id: "3",
     img: "https://res.cloudinary.com/don03rsnm/image/upload/v1776105247/Welcome_to_Departure_Service_rxvulw.png",
     url: "https://example.com/three",
-    height: 600,
+    height: 750,
+  },
+
+  {
+    id: "4",
+    img: "https://res.cloudinary.com/don03rsnm/image/upload/v1787855210/Artboard_1_v5_cmvtbz.png",
+    url: "https://example.com/three",
+    height: 400,
   },
 ];
