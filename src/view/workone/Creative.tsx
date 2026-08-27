ca47b4965334958696d2688d0e2a7a4b38806520
Emmanuel Sani <emmanuelsani2006@gmail.com>
2026-08-27 19:26:11 +0100

    fix: Masonary Animation plays when in view

--- src/view/workone/Creative.tsx
+++ src/view/workone/Creative.tsx
@@ -1,20 +1,30 @@
+import { useRef } from "react";
+import { useInView } from "framer-motion";
+
 import Masonry from "../../components/Masonry";
 import { work } from "./GraphicWorks";
 
 const Creative = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const isInView = useInView(containerRef, { once: true, amount: 0.2 });
   return (
     <>
-      <Masonry
-        items={work}
-        ease="bounce.out"
-        duration={1.1}
-        stagger={0.07}
-        animateFrom="random"
-        scaleOnHover
-        hoverScale={0.95}
-        blurToFocus
-        colorShiftOnHover={false}
-      />
+      <div ref={containerRef} className="w-full min-h-150">
+        {isInView && (
+          <Masonry
+            items={work}
+            ease="bounce.out"
+            duration={1.1}
+            stagger={0.07}
+            animateFrom="bottom"
+            scaleOnHover
+            hoverScale={0.95}
+            blurToFocus
+            colorShiftOnHover={false}
+          />
+        )}
+      </div>
     </>
   );
 };
