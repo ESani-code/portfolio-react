c2703570769ecf8f9efd0258875da5f0d4f47169
Emmanuel Sani <emmanuelsani2006@gmail.com>
2026-08-30 02:21:47 +0100

    feat: Added Depth arousel for Moblie View

--- src/view/workone/Creative.tsx
+++ src/view/workone/Creative.tsx
@@ -3,14 +3,54 @@ import { useInView } from "framer-motion";
 
 import Masonry from "../../components/Masonry";
 import { work } from "./GraphicWorks";
+import DepthCarousel from "../../components/DepthCarousel";
 
 const Creative = () => {
   const containerRef = useRef<HTMLDivElement>(null);
 
-  const isInView = useInView(containerRef, { once: true, amount: 0.2 });
+  const isInView = useInView(containerRef, { once: true, amount: 0.5 });
+
+  const graphics = [
+    {
+      image:
+        "https://res.cloudinary.com/don03rsnm/image/upload/v1787336142/Export_rpaktx.png",
+      alt: "One",
+    },
+    {
+      image:
+        "https://res.cloudinary.com/don03rsnm/image/upload/v1787336118/Export_2_b2h9ga.png",
+      alt: "Two",
+    },
+    {
+      image:
+        "https://res.cloudinary.com/don03rsnm/image/upload/v1776105247/Welcome_to_Departure_Service_rxvulw.png",
+      alt: "Three",
+    },
+    {
+      image:
+        "https://res.cloudinary.com/don03rsnm/image/upload/v1787855210/Artboard_1_v5_cmvtbz.png",
+      alt: "Four",
+    },
+    {
+      image:
+        "https://res.cloudinary.com/don03rsnm/image/upload/v1787855243/Welcome_Back_to_School_2_npwzgr.png",
+      alt: "Five",
+    },
+    {
+      image:
+        "https://res.cloudinary.com/don03rsnm/image/upload/v1767571873/gwork12_fvw8qq.jpg",
+      alt: "Six",
+    },
+    {
+      image:
+        "https://res.cloudinary.com/don03rsnm/image/upload/v1787855233/Website_Launch_V2_vlfsyb.png",
+      alt: "Seven",
+    },
+  ];
   return (
     <>
-      <div ref={containerRef} className="w-full min-h-150">
+      {/* Only Visible for Desktop Deveices */}
+      <div ref={containerRef} className="w-full px-6 min-h-150 hidden lg:block">
         {isInView && (
           <Masonry
             items={work}
@@ -19,11 +59,37 @@ const Creative = () => {
             stagger={0.07}
             animateFrom="bottom"
             scaleOnHover
-            hoverScale={0.95}
+            hoverScale={1.05}
             blurToFocus
             colorShiftOnHover={false}
           />
         )}
+      </div>
+
+      {/* Only Visible for Mobile Deveices */}
+      <div className="h-160 p-4 block lg:hidden">
+        <DepthCarousel
+          items={graphics}
+          depth={220}
+          spread={150}
+          tilt={22}
+          tiltDirection="right"
+          perspective={1400}
+          visibleCards={4}
+          falloff={0.2}
+          blur={2}
+          autoplay={false}
+          // loop
+          cardWidth={400}
+          cardHeight={500}
+          radius={10}
+          tint="#05060a"
+          duration={700}
+          ease="power3.out"
+          autoplayDelay={3200}
+          showControls
+          showIndicators
+        />
       </div>
     </>
   );
--- src/view/workone/GraphicWorks.ts
+++ src/view/workone/GraphicWorks.ts
@@ -3,19 +3,19 @@ export const work = [
     id: "1",
     img: "https://res.cloudinary.com/don03rsnm/image/upload/v1787336142/Export_rpaktx.png",
     url: "https://example.com/one",
-    height: 750,
+    height: 800,
   },
   {
     id: "2",
     img: "https://res.cloudinary.com/don03rsnm/image/upload/v1787336118/Export_2_b2h9ga.png",
     url: "https://example.com/two",
-    height: 750,
+    height: 800,
   },
   {
     id: "3",
     img: "https://res.cloudinary.com/don03rsnm/image/upload/v1776105247/Welcome_to_Departure_Service_rxvulw.png",
     url: "https://example.com/three",
-    height: 750,
+    height: 800,
   },
 
   {
