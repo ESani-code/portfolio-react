efe55e307cccff874a482a8b1cfc01b896b98d3a
Emmanuel Sani <emmanuelsani2006@gmail.com>
2026-08-30 14:08:16 +0100

    refactor: Moved Graphics data into Graphics Works & added more works

--- src/view/workone/Creative.tsx
+++ src/view/workone/Creative.tsx
@@ -2,23 +2,13 @@ import { useRef } from "react";
 import { useInView } from "framer-motion";
 
 import Masonry from "../../components/Masonry";
-import { work } from "./GraphicWorks";
+import { work, graphics } from "./GraphicWorks";
 import Carousel from "./Carousel";
 
 const Creative = () => {
   const containerRef = useRef<HTMLDivElement>(null);
 
   const isInView = useInView(containerRef, { once: true, amount: 0.5 });
-
-  const graphics = [
-    "https://res.cloudinary.com/don03rsnm/image/upload/v1787336142/Export_rpaktx.png",
-    "https://res.cloudinary.com/don03rsnm/image/upload/v1787336118/Export_2_b2h9ga.png",
-    "https://res.cloudinary.com/don03rsnm/image/upload/v1776105247/Welcome_to_Departure_Service_rxvulw.png",
-    "https://res.cloudinary.com/don03rsnm/image/upload/v1787855210/Artboard_1_v5_cmvtbz.png",
-    "https://res.cloudinary.com/don03rsnm/image/upload/v1787855243/Welcome_Back_to_School_2_npwzgr.png",
-    "https://res.cloudinary.com/don03rsnm/image/upload/v1767571873/gwork12_fvw8qq.jpg",
-    "https://res.cloudinary.com/don03rsnm/image/upload/v1787855233/Website_Launch_V2_vlfsyb.png",
-  ];
 
   return (
     <>
