d92e49e7323cd8449448f0943baeb6eb9cf58d93
Emmanuel Sani <emmanuelsani2006@gmail.com>
2026-08-31 11:02:48 +0100

    fix: Styling Issues

--- src/view/hero/ContactBar.tsx
+++ src/view/hero/ContactBar.tsx
@@ -14,14 +14,14 @@ const ContactBar = () => {
   return (
     <div className="absolute inset-0 z-100 w-full h-10 flex items-center justify-between px-8 pt-8">
       <img
-        className="h-12"
+        className="h-18"
         src="https://res.cloudinary.com/don03rsnm/image/upload/v1788169036/fsdgArtboard_1_copy_2_8x_q6zsw7.png"
         alt="Logo PNG"
       />
       <div className="items-end">
         {links.map(({ link, icon }) => (
           <a className="pl-8" href={link}>
-            <i className={`bi text-2xl ${icon}`} />
+            <i className={`bi text-3xl ${icon}`} />
           </a>
         ))}
       </div>
