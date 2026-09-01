651d72f5c8d27823d6c956899b8cba1972c5c314
Emmanuel Sani <emmanuelsani2006@gmail.com>
2026-09-01 02:43:29 +0100

    feat: Added Spring Bounce to Contact Bar

--- src/view/hero/ContactBar.tsx
+++ src/view/hero/ContactBar.tsx
@@ -1,5 +1,6 @@
 import { creativeLinks, developerLinks } from "./Links";
 import { useTheme } from "../../context/ThemeContext";
+import { Magnetic } from "./MagneticCursor";
 
 const ContactBar = () => {
   const { theme } = useTheme();
@@ -13,13 +14,21 @@ const ContactBar = () => {
           alt="Logo PNG"
         />
       </a>
+
       <div className="items-end pointer-events-auto">
         {links.map(({ link, icon }) => (
-          <a className=" pl-8 lg:pl-12" href={link}>
-            <i
-              className={`bi text-3xl lg:text-4xl ${icon} hover:text-accent-color transistion-all duration-200 ease-in-out`}
-            />
-          </a>
+          <Magnetic
+            magneticDistance={110}
+            strength={0.35}
+            stiffness={60}
+            damping={5}
+          >
+            <a className=" pl-8 lg:pl-12" href={link}>
+              <i
+                className={`bi text-3xl lg:text-4xl ${icon} hover:text-accent-color transistion-all duration-200 ease-in-out`}
+              />
+            </a>
+          </Magnetic>
         ))}
       </div>
     </div>
