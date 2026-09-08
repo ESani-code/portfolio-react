87801f14cf7272391030931390731c74db3013cb
Emmanuel Sani <emmanuelsani2006@gmail.com>
2026-09-09 00:31:02 +0100

    fix: Styling Issues

--- src/view/worktwo/Bento.tsx
+++ src/view/worktwo/Bento.tsx
@@ -9,7 +9,7 @@ export const Bento = () => {
   return (
     <section
       id="free-tools"
-      className="bg-sidebar rounded-3xl p-4 my-16 max-w-6xl mx-auto"
+      className=" bg-sidebar rounded-lg md:px-8 lg:px-12 py-4 my-5 max-w-8xl mx-auto"
     >
       <div className="flex flex-col md:flex-row items-end justify-between w-full">
         <div className="flex flex-col my-12 w-full items-start justify-start gap-4">
@@ -103,8 +103,8 @@ export const Bento = () => {
           className="pointer-events-none flex items-center justify-center overflow-hidden md:hover:scale-105 hover:shadow-[-6px_6px_32px_8px_rgba(192,192,192,0.2)] hover:-rotate-6 transition-all duration-200 ease-in-out relative bg-primary/20 h-[330px] rounded-xl flex flex-col items-center justify-center px-5 py-6"
         >
           <p className="rotate-6 mb-1 text-base-content">Playbooks</p>
-          <h3 className="rotate-6 text-2xl font-semibold text-center px-6 py-2 bg-white/75 rounded-full">
-            Comming Soon
+          <h3 className="rotate-6 text-2xl font-semibold text-center px-6 py-2 bg-accent-color rounded-full">
+            View More
           </h3>
         </a>
       </div>
