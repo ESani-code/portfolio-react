759d3ab08a8fc879289d046471a6f143996ab8b8
Emmanuel Sani <emmanuelsani2006@gmail.com>
2026-09-08 23:51:42 +0100

    fix: Styling Issues

--- src/view/hero/ContactBar.tsx
+++ src/view/hero/ContactBar.tsx
@@ -6,23 +6,25 @@ const ContactBar = () => {
   const { theme } = useTheme();
   const links = theme == "creative" ? creativeLinks : developerLinks;
   return (
-    <div className="absolute inset-0 z-100 w-full h-10 flex items-center justify-between px-6 lg:px-8 pt-12 pointer-events-none">
-      <Magnetic
-        magneticDistance={120}
-        strength={0.2}
-        stiffness={80}
-        damping={8}
-      >
-        <a className="pointer-events-none" href="/#">
-          <img
-            className="h-16 md:h-18 lg:h-22 w-auto"
-            src="https://res.cloudinary.com/don03rsnm/image/upload/v1788169036/fsdgArtboard_1_copy_2_8x_q6zsw7.png"
-            alt="Logo PNG"
-          />
-        </a>
-      </Magnetic>
+    <div className="absolute inset-0 z-100 w-full h-10 flex items-center justify-center md:justify-between px-6 lg:px-8 pt-12 pointer-events-none">
+      <div className="hidden md:block">
+        <Magnetic
+          magneticDistance={120}
+          strength={0.2}
+          stiffness={80}
+          damping={8}
+        >
+          <a className="pointer-events-none" href="/#">
+            <img
+              className="h-16 md:h-18 lg:h-22 w-auto"
+              src="https://res.cloudinary.com/don03rsnm/image/upload/v1788169036/fsdgArtboard_1_copy_2_8x_q6zsw7.png"
+              alt="Logo PNG"
+            />
+          </a>
+        </Magnetic>
+      </div>
 
-      <div className="items-end pointer-events-auto">
+      <div className="items-center md:items-end pointer-events-auto">
         {links.map(({ link, icon }) => (
           <Magnetic
             magneticDistance={120}
