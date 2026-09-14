5db5855a080033c9c6406fba07442ddcb380d089
Emmanuel Sani <emmanuelsani2006@gmail.com>
2026-09-14 21:48:53 +0100

    fix: Animation Timing

--- src/components/AnimateMode.tsx
+++ src/components/AnimateMode.tsx
@@ -18,10 +18,10 @@ export const AnimatedMode = ({
     <AnimatePresence mode="wait">
       <motion.div
         key={theme}
-        initial={{ opacity: 0, y: 20 }}
+        initial={{ opacity: 0, y: 30 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, x: -20 }}
-        transition={{ duration: 0.4, ease: "easeInOut" }}
+        transition={{ duration: 1.7, ease: "easeInOut" }}
         className={`${className} w-full`}
       >
         {theme === "developer" ? developer : creative}
