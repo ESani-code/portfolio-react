d54e25b3899bcb8fff11a216041d309b543d0e48
Emmanuel Sani <emmanuelsani2006@gmail.com>
2026-09-24 22:08:18 +0100

    fix: Work Links

--- src/view/worktwo/motiondesigns.ts
+++ src/view/worktwo/motiondesigns.ts
@@ -27,29 +27,18 @@ export const motionDesigns: motionDesigType[] = [
     title: "First Work",
     tools: "Blender, After Effects",
     href: href,
-    colSpan: "2",
+    colSpan: "1",
     rowSpan: "1",
     hoverRotate: "-rotate-3",
     thumbnail:
-      "https://res.cloudinary.com/don03rsnm/image/upload/v1790212537/vlcsnap-2026-09-24-02h15m31s975_eaezok.jpg",
-    alt: "Pinterest Work",
+      "https://res.cloudinary.com/don03rsnm/image/upload/v1790212504/vlcsnap-2026-09-24-02h14m58s654_jdmb9o.jpg",
+    alt: "Devspace",
   },
   {
     title: "First Work",
     tools: "Blender, After Effects",
     href: href,
-    colSpan: "2",
-    rowSpan: "1",
-    hoverRotate: "rotate-3",
-    thumbnail:
-      "https://res.cloudinary.com/don03rsnm/image/upload/v1790212537/vlcsnap-2026-09-24-02h15m31s975_eaezok.jpg",
-    alt: "Pinterest Work",
-  },
-  {
-    title: "First Work",
-    tools: "Blender, After Effects",
-    href: href,
-    colSpan: "2",
+    colSpan: "1",
     rowSpan: "1",
     hoverRotate: "rotate-3",
     thumbnail:
@@ -61,11 +50,22 @@ export const motionDesigns: motionDesigType[] = [
     tools: "Blender, After Effects",
     href: href,
     colSpan: "1",
+    rowSpan: "1",
+    hoverRotate: "rotate-3",
+    thumbnail:
+      "https://res.cloudinary.com/don03rsnm/image/upload/v1790212459/vlcsnap-2026-09-24-02h14m14s039_ajh1pj.jpg",
+    alt: "Gemini Personal Project",
+  },
+  {
+    title: "First Work",
+    tools: "Blender, After Effects",
+    href: href,
+    colSpan: "1",
     rowSpan: "2",
     hoverRotate: "rotate-3",
     thumbnail:
-      "https://res.cloudinary.com/don03rsnm/image/upload/v1790212709/vlcsnap-2026-09-24-02h18m22s283_pd0pbw.jpg",
-    alt: "Monalisa Illustration",
+      "https://res.cloudinary.com/don03rsnm/image/upload/v1790212309/vlcsnap-2026-09-24-02h10m40s654_avmx4x.jpg",
+    alt: "CUFS Conference",
   },
   {
     title: "First Work",
@@ -75,8 +75,8 @@ export const motionDesigns: motionDesigType[] = [
     rowSpan: "1",
     hoverRotate: "rotate-3",
     thumbnail:
-      "https://res.cloudinary.com/don03rsnm/image/upload/v1790212537/vlcsnap-2026-09-24-02h15m31s975_eaezok.jpg",
-    alt: "Pinterest Work",
+      "https://res.cloudinary.com/don03rsnm/image/upload/v1790212413/vlcsnap-2026-09-24-02h13m29s435_qi6txo.jpg",
+    alt: "Chowcheck Intro Video",
   },
   {
     title: "First Work",
